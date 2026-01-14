048c5a96969678eb4306d97e09b15488a4dc48ce
hacker12345227 <Jarno.eulen12@gmail.com>
2026-01-14 22:01:26 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -2,19 +2,16 @@ import mongoose from "mongoose";
 
 mongoose.connect(process.env.MONGO_URI);
 
-const Product = mongoose.models.Product || mongoose.model(
-  "Product",
-  { name: String, price: Number }
-);
+const Product = mongoose.models.Product || mongoose.model("Product", { name:String, price:Number });
 
-export default async function handler(req, res) {
-  if (req.method === "GET") {
+export default async function handler(req,res){
+  if(req.method==='GET'){
     const products = await Product.find();
-    return res.status(200).json(products);
+    return res.json(products);
   }
-
-  if (req.method === "POST") {
-    const product = await Product.create(req.body);
-    return res.status(201).json(product);
+  if(req.method==='POST'){
+    const p = await Product.create(req.body);
+    return res.status(201).json(p);
   }
+  res.status(405).end();
 }
